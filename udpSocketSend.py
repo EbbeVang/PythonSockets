2e8300154299344a8da525b8a93fbbd1f00c45ac
Ebbe Vang <ev@evang.dk>
2021-02-15 20:16:55 +0100

    add udp tcp examples

---
+++ udpSocketSend.py
@@ -0,0 +1,16 @@
+'''
+Interactive Digital systems
+Sending UDP packets
+'''
+
+import socket 
+UDP_IP = "127.0.0.1"
+UDP_PORT = 7007
+MESSAGE = b"Hello, World!"
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+ 
+sock = socket.socket(socket.AF_INET, # Internet
+socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
